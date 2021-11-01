c16356a8875a77f4d44d01305be7b4c9897ba41c
Shehzaan-Mansuri <shehzaanmansuri1@gmail.com>
2021-11-01 12:27:07 +0530

    FlutterLogo

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Logo"),
+      ),
+      body: Center(
+        child: FlutterLogo(
+          size: 100,
+          style: FlutterLogoStyle.stacked,
+        ),
+      ),
+    );
+  }
+}
